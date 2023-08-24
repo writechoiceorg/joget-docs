096af0223325c0c38a5ba76153db88052aa94a1e
Heitor <heitor@writechoice.com.br>
2023-08-24 09:57:52 -0300

    chore(getting started): update onBrokenLinks to just warn

--- joget/docusaurus.config.js
+++ joget/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   projectName: 'joget-docs', // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
